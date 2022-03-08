6f57bd726bfcc0831322de1eb9f17e5e7a13bf9f
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-03-08 15:01:30 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,63 @@
+var myDate = new Date();
+            var hrs = myDate.getHours();
+        
+            var greet;
+        
+            if (hrs < 10)
+                greet = 'Selamat Pagi';
+            else if (hrs >= 10 && hrs <= 15)
+                greet = 'Selamat Siang';
+            else if (hrs >= 15 && hrs <= 18)
+                greet = 'Selamat Sore';
+            else if (hrs >= 18 && hrs <= 19)
+                greet = 'Selamat Petang';
+            else if (hrs >= 19 && hrs <= 24)
+                greet = 'Selamat Malam';
+        
+            document.getElementById('welcome').innerHTML =
+                '<b>' + greet + '</b> ';
+
+                // Your web app's Firebase configuration
+      var firebaseConfig = {
+        apiKey: "AIzaSyCG_92OLetfydA6KDg2kwweJF4bf4hG7Jw",
+        authDomain: "joxiplayer-site.firebaseapp.com",
+        databaseURL: "https://joxiplayer-site-default-rtdb.firebaseio.com",
+        projectId: "joxiplayer-site",
+        storageBucket: "joxiplayer-site.appspot.com",
+        messagingSenderId: "288570493379",
+        appId: "1:288570493379:web:8c1168c876624cc6a95576",
+        measurementId: "G-4J03BRQ7Y1"
+      };
+      firebase.initializeApp(firebaseConfig);
+
+      firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+          document.getElementById("username").innerHTML = `
+                <p>${user.displayName}
+            `;
+        document.getElementById("email").innerHTML = `
+                <p>${user.email}
+            `;
+          document.getElementById("username-link").innerHTML = `${user.uid}
+            `;
+            const linkUsername = document.getElementById("username-link");
+            const link = document.getElementById("link");
+            link.href = link.textContent ;
+
+        }   else {
+          window.location = "login/index.html";
+        }
+      });
+
+      function logout() {
+        firebase
+          .auth()
+          .signOut()
+          .then(function () {
+            window.location = "login/index.html";
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+      showData();
